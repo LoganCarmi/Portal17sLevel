481cba1d3532f682c05f7a5d519768e18f187d56
LoganCarmi <logan.carminati@ynov.com>
2023-02-20 11:49:23 +0100

    Téléportation au travers des portails + Rotation
    Ajout de la fonctionnalité pour maintenant passer au travers des portails horizontaux pour le joueur

--- Assets/Scripts/PortalGunScripts/BallScript.cs
+++ Assets/Scripts/PortalGunScripts/BallScript.cs
@@ -7,6 +7,7 @@ public class BallScript : MonoBehaviour
     [SerializeField] float Force = 50f;
     [SerializeField] GameObject Portal;
     Rigidbody rb;
+    Quaternion PortalRotation;
 
     void Start()
     {
@@ -18,11 +19,27 @@ public class BallScript : MonoBehaviour
     private void OnCollisionEnter(Collision other) {
         
         if (other.collider.tag == "Ground" || other.collider.tag == "Wall") {
+
+            if (other.collider.tag == "Wall") {
+                Quaternion rotation = other.gameObject.transform.rotation;
+                float angle90 = Quaternion.Angle(rotation, Quaternion.Euler(0, 90, 0));
+                float angle270 = Quaternion.Angle(rotation, Quaternion.Euler(0, 270, 0));
+                float angle0 = Quaternion.Angle(rotation, Quaternion.Euler(0, 360, 0));
+
+
+                if (Mathf.Approximately(angle90, 0f)) PortalRotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                else if (Mathf.Approximately(angle270, 0f)) PortalRotation = Quaternion.Euler(new Vector3(0, 270, 0));
+                else if (Mathf.Approximately(angle0, 0f)) PortalRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                else PortalRotation = Quaternion.Euler(new Vector3(0, 360, 0));;
+            } else {
+                PortalRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            }
+
             
-            if ( GameObject.FindWithTag(Portal.tag) == null ) Instantiate(Portal, gameObject.transform.position, Quaternion.identity);
+            if ( GameObject.FindWithTag(Portal.tag) == null ) Instantiate(Portal, gameObject.transform.position, PortalRotation);
             else {
                 Destroy(GameObject.FindWithTag(Portal.tag));
-                Instantiate(Portal, gameObject.transform.position, Quaternion.identity);
+                Instantiate(Portal, gameObject.transform.position, PortalRotation);
             }
             Destroy(gameObject);
         }
--- Assets/Scripts/PortalGunScripts/Portal.cs
+++ Assets/Scripts/PortalGunScripts/Portal.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    
 
+    string OtherPortalTag;
 
-    void Update()
-    {
-        
+    private void Start() {
+        if(gameObject.CompareTag("OrangePortal")) OtherPortalTag = "BluePortal";
+        else OtherPortalTag = "OrangePortal";
     }
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.CompareTag("Player") && GameObject.FindWithTag(OtherPortalTag) != null) {
+            other.GetComponent<CharacterController>().enabled = false;
+            // Calculer la nouvelle position du joueur
+            Vector3 new_position = GameObject.FindWithTag(OtherPortalTag).transform.position + 2 * GameObject.FindWithTag(OtherPortalTag).transform.right;
+
+
+            other.transform.rotation = Quaternion.Euler(GameObject.FindWithTag(OtherPortalTag).transform.rotation.eulerAngles + new Vector3(0, 90, 0));
+
+            // Téléporter le joueur
+            other.gameObject.transform.position = new_position;
+            other.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+
 }
